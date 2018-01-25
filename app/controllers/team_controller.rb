18b3cadf0a7c651100a87689d63f22efa68d22de
EC2 Default User <ec2-user@ip-172-31-38-157.us-east-2.compute.internal>
2018-01-25 22:34:55 +0000

    edit team route

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -36,4 +36,28 @@ class TeamController < ApplicationController
     end
   end
   
+  get '/teams/:id/edit' do 
+      if Helpers.logged_in?(session) 
+        @team = Team.find(params[:id])
+          if Helpers.current_user(session).id == @team.user_id
+            erb :'teams/edit_team'
+          else
+            redirect to '/teams'
+          end
+        else
+          redirect to '/login'
+      end
+  end
+  
+  patch '/teams/:id' do
+    team = Team.find(params[:id])
+    team.update(name: params[:name],chaser: params[:chaser],beater: params[:beater],keeper: params[:keeper],seeker: params[:seeker])
+
+    if team.name != ""
+      redirect to "teams/#{team.id}"
+    else
+      redirect to "teams/#{team.id}/edit"
+    end
+  end
+  
 end
