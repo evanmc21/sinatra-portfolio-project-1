66862bc572c0a567c5e8124c2631f958e77c5eb4
EC2 Default User <ec2-user@ip-172-31-38-157.us-east-2.compute.internal>
2018-01-25 21:34:12 +0000

    new team form

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -9,4 +9,22 @@ class TeamController < ApplicationController
         end
     end
     
+    get '/teams/new' do
+        if Helpers.logged_in?(session)
+          erb :'/teams/create_team'
+        else
+          redirect to '/login'
+        end
+    end
+    
+    post '/teams' do
+        team = Team.create(name: params[:name],chaser: params[:chaser],beater: params[:beater],keeper: params[:keeper],seeker: params[:seeker], user_id: session[:user_id])
+    
+        if team.name != ""
+          redirect to "teams/#{team.id}"
+        else
+          redirect to '/teams/new'
+        end
+  end
+    
 end
