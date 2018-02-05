86afc484845c6b992697f74ac9b98792ee1723ea
EC2 Default User <ec2-user@ip-172-31-38-157.us-east-2.compute.internal>
2018-02-05 21:44:17 +0000

    fixed team error

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,61 +1,59 @@
 class TeamController < ApplicationController
     
-    get '/teams' do
-        @teams = Team.all
-        if Helpers.logged_in?(session)
-          erb :'teams/teams_index'
-        else
-          redirect to '/login'
-        end
-    end
-    
-    get '/teams/new' do
-        if Helpers.logged_in?(session)
-          erb :'/teams/create_team'
-        else
-          redirect to '/login'
-        end
-    end
-    
-    post '/teams' do
-        @team = Team.create(name: params[:name],chaser: params[:chaser],beater: params[:beater],keeper: params[:keeper],seeker: params[:seeker], user_id: session[:user_id])
-        @team.user = Helpers.current_user(session)
-        @team.save
-        redirect '/teams'
-    end
-    
-    get '/teams/:id' do
+  get '/teams' do
+      @teams = Team.all
       if Helpers.logged_in?(session)
-        @team = Team.find_by(id: params[:id])
-        erb :'teams/show_team'
+        erb :'teams/teams_index'
       else
         redirect to '/login'
       end
-    end
-
+  end
   
-  get '/teams/:id/edit' do 
-      if Helpers.logged_in?(session) 
-        @team = Team.find_by(id: params[:id])
-          if Helpers.current_user(session).id == @team.user_id
-            erb :'teams/edit_team'
-          else
-            redirect '/login'
-          end
+  get '/teams/new' do
+      if Helpers.logged_in?(session)
+        erb :'/teams/create_team'
+      else
+        redirect to '/login'
       end
   end
   
+  post '/teams' do
+      @team = Team.create(name: params[:name],chaser: params[:chaser],beater: params[:beater],keeper: params[:keeper],seeker: params[:seeker], user_id: session[:user_id])
+      @team.user = Helpers.current_user(session)
+      @team.save
+      redirect '/teams'
+  end
+  
+  get '/teams/:id' do
+    if Helpers.logged_in?(session)
+      @team = Team.find_by(id: params[:id])
+      erb :'teams/show_team'
+    else
+      redirect to '/login'
+    end
+  end
+
+
+  get '/teams/:id/edit' do 
+      if Helpers.logged_in?(session) 
+        @team = Team.find_by(id: params[:id])
+          erb :'teams/edit_team'
+      else
+          redirect '/login'
+      end
+  end
+
   patch '/teams/:id' do
     @team = Team.find_by(id: params[:id])
     if Helpers.current_user(session) == @team.user
-    @team.update(name: params[:name],chaser: params[:chaser],beater: params[:beater],keeper: params[:keeper],seeker: params[:seeker])
-    @team.save
-      redirect to "teams/#{@team.id}"
+      @team.update(name: params[:name],chaser: params[:chaser],beater: params[:beater],keeper: params[:keeper],seeker: params[:seeker])
+      @team.save
+        redirect to "/teams/#{@team.id}"
     else
-      erb :'/teams/team_error'
+        erb :'/teams/team_error'
     end
   end
-  
+
   delete '/teams/:id/delete' do
     @team = Team.find_by(id: params[:id])
     if Helpers.current_user(session) == @team.user
@@ -64,6 +62,12 @@ class TeamController < ApplicationController
     else
        erb :'/teams/team_error'
     end
+  end
+  
+  post '/teams/:id' do
+    @team = Team.find_by(params[:username])
+    @team.save
+    redirect to "teams/#{@team.id}"
   end
 
 end
